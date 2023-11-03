177e54b026d445bcf0d23868066eda49eca8db76
Laura Robinson <laura3918@hotmail.co.uk>
2023-11-03 17:52:41 +0000

    Feat: Added tests for convertToNumerals

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -21,6 +21,27 @@ describe("convertToNumerals", () => {
     expect(convertToNumerals(86)).toBe("LXXXVI");
     expect(convertToNumerals(99)).toBe("XCIX");
   });
+
+  test("Convert an integer between 100-999 into roman numerals", () => {
+    expect(convertToNumerals(111)).toBe("CXI");
+    expect(convertToNumerals(214)).toBe("CCXIV");
+    expect(convertToNumerals(315)).toBe("CCCXV");
+    expect(convertToNumerals(430)).toBe("CDXXX");
+    expect(convertToNumerals(541)).toBe("DXLI");
+    expect(convertToNumerals(664)).toBe("DCLXIV");
+    expect(convertToNumerals(775)).toBe("DCCLXXV");
+    expect(convertToNumerals(886)).toBe("DCCCLXXXVI");
+    expect(convertToNumerals(999)).toBe("CMXCIX");
+  });
+
+  test("Convert an integer between 1000-3000 into roman numerals", () => {
+    expect(convertToNumerals(1000)).toBe("M");
+    expect(convertToNumerals(1356)).toBe("MCCCLVI");
+    expect(convertToNumerals(2222)).toBe("MMCCXXII");
+    expect(convertToNumerals(2545)).toBe("MMDXLV");
+    expect(convertToNumerals(2999)).toBe("MMCMXCIX");
+    expect(convertToNumerals(3000)).toBe("MMM");
+  });
 });
 
 describe("convertToInt", () => {
